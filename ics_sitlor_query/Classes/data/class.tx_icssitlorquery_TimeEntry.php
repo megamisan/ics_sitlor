afc4d7523e515c90bc17a8391ef5d6dfc637d27c
tsimomo <tsimomo@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-26 09:42:03 +0000

    Time entry object
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_sitlor/trunk@56714 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ics_sitlor_query/Classes/data/class.tx_icssitlorquery_TimeEntry.php
@@ -0,0 +1,55 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2011 In Cite Solution <technique@in-cite.net>
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+/**
+ * [CLASS/FUNCTION INDEX of SCRIPT]
+ *
+ * Hint: use extdeveval to insert/update function index above.
+ */
+
+
+/**
+ * Class 'tx_icssitlorquery_TimeEntry' for the 'ics_sitlor_query' extension.
+ *
+ * @author	Tsi YANG <tsi@in-cite.net>
+ * @package	TYPO3
+ * @subpackage	tx_icssitlorquery
+ */
+
+class tx_icssitlorquery_TimeEntry{
+	private $dayOfWeek;	// int : ISO-8601 numeric representation of the day of the week, 1 (for Monday) through 7 (for Sunday)
+	private $start;	// int
+	private $end;	// int
+	private $isPM;	// bool
+	
+	public function __construct($dow, $start=0, $end=0, $isPM=false) {
+		if (!is_int($dow))
+			throw new Exception('Time entry Day of week must be integer.');
+
+		$this->dayOfWeek = $dow;
+		$this->start = $start;
+		$this->end = $end;
+		$this->isPM = $isPM;
+	}
+
+}
