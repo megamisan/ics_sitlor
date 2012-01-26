abce5208763815b59b984876b26c6c4e8eaa27a6
tsimomo <tsimomo@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-26 14:33:24 +0000

    Abstract restaurant implemented
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ics_sitlor/trunk@56744 735d13b6-9817-0410-8766-e36946ffe9aa

--- ics_sit_query/Classes/class.tx_icssitquery_AbstractRestaurant.php
+++ ics_sit_query/Classes/class.tx_icssitquery_AbstractRestaurant.php
@@ -30,6 +30,76 @@
  * @subpackage	tx_icssitquery
  */
  
-abstract class tx_icssitquery_AbstractRestaurant extends AbstractData{
-
+abstract class tx_icssitquery_AbstractRestaurant extends tx_icssitquery_AbstractData{
+	private $type;		// Type of restaurant
+	private $category;	// Category of restaurant
+	private $address;	// Address of restaurant
+	private $zip;		// Location zip
+	private $city;		// City
+	private $illustration;	// Illustration of restaurant
+	private $chainAndLabel;	// Restaurant chain or label
+	
+	/**
+	 * Retrieves properties
+	 *
+	 * @param	string $name : Property's name
+	 *
+	 * @return mixed : name 's value
+	 */
+	public function __get($name) {
+		switch ($name) {
+			case 'Type':
+				return $this->type;
+			case 'Category':
+				return $this->category;
+			case 'Address':
+				return $this->address;
+			case 'Zip';
+				return $this->zip;
+			case 'City':
+				return $this->city;
+			case 'Illustration':
+				return $this->illustration;
+			case 'ChainAndLabel':
+				return $this->chainAndLabel;
+			default : 
+				return parent::__get($name);
+		}
+	}
+	
+	/**
+	 * Set name
+	 *
+	 * @param	string $name : Property's name
+	 * @param	mixed : Property's value
+	 *
+	 * @return void
+	 */
+	public function __set($name, $value) {
+		switch ($name) {
+			case 'Type':
+				$this->type = $value;
+			break;
+			case 'Category':
+				$this->category = $value;
+			break;
+			case 'Address':
+				$this->address = $value;
+			break;
+			case 'Zip':
+				$this->zip = $value;
+			break;
+			case 'City':
+				$this->city = $value;
+			break;
+			case 'Illustration':
+				$this->illustration = $value;
+			break;
+			case 'ChainAndLabel':
+				$this->chainAndLabel = $value;
+			break;
+			default :
+				parent::__set($name, $value);
+		}
+	}	
 }
